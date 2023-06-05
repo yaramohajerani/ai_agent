cceb2292be8ee9387fbbab539354f596db80800b
yaramohajerani <yaramohajerani@gmail.com>
2023-06-04 23:52:53 -0400

    route langchain stdout to streamlit and get rid of password page

--- zappier_agent_portal.py
+++ zappier_agent_portal.py
@@ -1,11 +1,15 @@
+from contextlib import redirect_stdout
+from io import StringIO
 from os import environ
-from langchain.llms import OpenAI
+from re import compile as re_compile
+
+import streamlit as st
 from langchain.agents import initialize_agent
+from langchain.agents.agent import AgentExecutor
 from langchain.agents.agent_toolkits import ZapierToolkit
 from langchain.agents.agent_types import AgentType
-from langchain.agents.agent import AgentExecutor
+from langchain.llms import OpenAI
 from langchain.utilities.zapier import ZapierNLAWrapper
-import streamlit as st
 
 
 @st.cache_resource
@@ -22,60 +26,28 @@ def setup_agent(openai_api_key: str, zapier_api_key: str) -> AgentExecutor:
                              agent=AgentType.ZERO_SHOT_REACT_DESCRIPTION,
                              verbose=True)
 
+    st.sidebar.title("Available Tools based on your Zapier API key:")
+    for tool in toolkit.get_tools():
+        st.sidebar.subheader(tool.name)
+        st.sidebar.write(tool.description)
+        st.sidebar.write("\n\n")
+
     return agent
 
 
-def page_footer() -> None:
-    """
-    display page footer
-
-    :return: None
-    """
-    st.markdown("""---""")
-    st.markdown("<center><p>Yara Mohajerani &copy; 2023. All Rights Reserved.</p></center>",
-                unsafe_allow_html=True)
-
-
-def check_password() -> bool:
-    """
-    Returns `True` if the user had the correct password.
-
-    :return: boolean for whether entered password is correct
-    """
-
-    def password_entered() -> None:
-        """
-        Checks whether a password entered by the user is correct.
-
-        :return: None
-        """
-        if st.session_state["password"] == st.secrets["password"]:
-            st.session_state["password_correct"] = True
-            del st.session_state["password"]  # don't store password
-        else:
-            st.session_state["password_correct"] = False
-
-    if "password_correct" not in st.session_state:
-        # First run, show input for password.
-        st.text_input(
-            "Password", type="password", on_change=password_entered, key="password"
-        )
-        page_footer()
-        return False
-    elif not st.session_state["password_correct"]:
-        # Password not correct, show input + error.
-        st.text_input(
-            "Password", type="password", on_change=password_entered, key="password"
-        )
-        st.error("Incorrect Password")
-        page_footer()
-        return False
-    else:
-        # Password correct.
-        return True
+def remove_ansi_escape_codes(text: str) -> str:
+    ansi_escape_pattern = re_compile(r'\x1B(?:[@-Z\\-_]|\[[0-?]*[ -/]*[@-~])')
+    return ansi_escape_pattern.sub('', text)
 
 
 def main():
+    col1, col2 = st.columns([5, 2])
+    col1.title("Welcome to your AI's central command! 🤖")
+    col1.caption("BETA")
+    col2.image(
+        "DALL·E 2023-06-04 14.20.46 - High resolution realistic image of a command room where a humanoid robot "
+        "sitting behind a large desk controls the world on a giant wall-to-wall screen.png")
+
     openai_api_key: str = st.sidebar.text_input("OPENAI API KEY", type="password")
     zapier_api_key: str = st.sidebar.text_input("ZAPIER NLA API KEY", type="password")
 
@@ -86,17 +58,30 @@ def main():
         user_prompt: str = st.text_input("Enter Prompt")
 
         if user_prompt.strip() != "":
-            st.info(agent.run(user_prompt))
+            stdout = StringIO()
+            try:
+                with redirect_stdout(stdout):
+                    agent.run(user_prompt)
+            except Exception as e:
+                st.write(f"Failure while executing: {e}")
+            finally:
+                # get output and remove ansi escape characters used for coloring of text in langchain
+                output_string: str = remove_ansi_escape_codes(stdout.getvalue())
 
-    page_footer()
+                # add new lines to make output easier to read
+                output_string = output_string.replace('...', '...\n\n')
+                output_string = output_string.replace('Action', '\n**Action**')
+                output_string = output_string.replace('Observation', '\n**Observation**')
+                output_string = output_string.replace('Thought', '\n**Thought**')
+                output_string = output_string.replace('Thought', '\n**Thought**')
+                output_string = output_string.replace('Final Answer', '\n**Final Answer**')
+
+                st.markdown(output_string, unsafe_allow_html=True)
+
+    st.markdown("""---""")
+    st.markdown("<center><p>Yara Mohajerani &copy; 2023. All Rights Reserved.</p></center>",
+                unsafe_allow_html=True)
 
 
 if __name__ == '__main__':
-    col1, col2 = st.columns([3, 2])
-    col1.title("Yara's Central AI Command 👹🤖")
-    col1.caption("BETA")
-    col2.image(
-        "DALL·E 2023-06-04 14.20.46 - High resolution realistic image of a command room where a humanoid robot "
-        "sitting behind a large desk controls the world on a giant wall-to-wall screen.png")
-    if check_password():
-        main()
+    main()
